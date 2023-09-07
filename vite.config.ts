1bc6a8464ee2fd78001ca869b4196a2b3b1d0940
Delaney Gillilan <delaneygillilan@gmail.com>
2023-09-06 20:03:19 -0700

    work on packaging stuff

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ import { resolve } from 'path'
 import { visualizer } from 'rollup-plugin-visualizer'
 import Unfonts from 'unplugin-fonts/vite'
 import { defineConfig, splitVendorChunkPlugin } from 'vite'
+import dts from 'vite-plugin-dts'
 
 export default defineConfig({
   plugins: [
@@ -40,10 +41,15 @@ export default defineConfig({
         },
       },
     }),
+    dts({
+      rollupTypes: true,
+      include: 'src/lib/**/*',
+    }),
   ],
   build: {
     target: 'esnext',
     minify: 'esbuild',
+    reportCompressedSize: true,
     sourcemap: true,
     lib: {
       entry: resolve(__dirname, 'src/lib/index.ts'),
