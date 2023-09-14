bce47ea7f761f4b5149d1402c9683014888f795e
Delaney Gillilan <delaneygillilan@gmail.com>
2023-09-13 17:09:25 -0700

    waiting for kids, new essays

--- library/src/lib/core.ts
+++ library/src/lib/core.ts
@@ -58,22 +58,6 @@ export function useProcessor({ regexp, replacer }: Preprocesser, str: string): s
   return str
 }
 
-// function cyrb53(str: string, seed = 0) {
-//   let h1 = 0xdeadbeef ^ seed,
-//     h2 = 0x41c6ce57 ^ seed
-//   for (let i = 0, ch; i < str.length; i++) {
-//     ch = str.charCodeAt(i)
-//     h1 = Math.imul(h1 ^ ch, 2654435761)
-//     h2 = Math.imul(h2 ^ ch, 1597334677)
-//   }
-//   h1 = Math.imul(h1 ^ (h1 >>> 16), 2246822507)
-//   h1 ^= Math.imul(h2 ^ (h2 >>> 13), 3266489909)
-//   h2 = Math.imul(h2 ^ (h2 >>> 16), 2246822507)
-//   h2 ^= Math.imul(h1 ^ (h1 >>> 13), 3266489909)
-
-//   return 4294967296 * (2097151 & h2) + (h1 >>> 0)
-// }
-
 const prefixHashes = new Map<string, string>()
 const pluginApplyFunctions = new Map<string, Function>()
 const pluginElementRegistry = new Map<Element, Set<string>>()
