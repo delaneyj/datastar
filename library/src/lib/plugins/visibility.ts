e4d90ecfaff747db175fde940efb48eee46fc83d
Delaney Gillilan <delaneygillilan@gmail.com>
2024-05-14 13:55:35 -0700

    initial fix

--- library/src/lib/plugins/visibility.ts
+++ library/src/lib/plugins/visibility.ts
@@ -110,13 +110,46 @@ export const TeleportPlugin: AttributePlugin = {
 // Scrolls the element into view
 export const ScrollIntoViewPlugin: AttributePlugin = {
   prefix: 'scrollIntoView',
-  onLoad: (ctx: AttributeContext) => {
-    const { el } = ctx
-    el.scrollIntoView({
+  mustHaveEmptyKey: true,
+  mustHaveEmptyExpression: true,
+  allowedModifiers: new Set([
+    'smooth',
+    'instant',
+    'auto',
+    'hstart',
+    'hcenter',
+    'hend',
+    'hnearest',
+    'vstart',
+    'vcenter',
+    'vend',
+    'vnearest',
+    'focus',
+  ]),
+
+  onLoad: ({ el, modifiers }: AttributeContext) => {
+    if (!el.tabIndex) el.setAttribute('tabindex', '0')
+    const opts: ScrollIntoViewOptions = {
       behavior: 'smooth',
       block: 'center',
       inline: 'center',
-    })
+    }
+    if (modifiers.has('smooth')) opts.behavior = 'smooth'
+    if (modifiers.has('instant')) opts.behavior = 'instant'
+    if (modifiers.has('auto')) opts.behavior = 'auto'
+    if (modifiers.has('hstart')) opts.inline = 'start'
+    if (modifiers.has('hcenter')) opts.inline = 'center'
+    if (modifiers.has('hend')) opts.inline = 'end'
+    if (modifiers.has('hnearest')) opts.inline = 'nearest'
+    if (modifiers.has('vstart')) opts.block = 'start'
+    if (modifiers.has('vcenter')) opts.block = 'center'
+    if (modifiers.has('vend')) opts.block = 'end'
+    if (modifiers.has('vnearest')) opts.block = 'nearest'
+
+    el.scrollIntoView(opts)
+    if (modifiers.has('focus')) el.focus()
+    delete el.dataset.focus
+    return () => el.blur()
   },
 }
 
