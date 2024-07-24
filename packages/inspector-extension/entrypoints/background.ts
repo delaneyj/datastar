448333ce2e56efe075b1062bc5df3c7543614548
Patrick Marchand <mail@patrickmarchand.com>
2024-07-24 11:56:52 -0400

    inspector-extension: improve types and allow devtools to message content

--- packages/inspector-extension/entrypoints/background.ts
+++ packages/inspector-extension/entrypoints/background.ts
@@ -1,30 +1,51 @@
-const ports = {};
-function connected(p) {
-    p.onMessage.addListener(async(m, s) => {
+
+const ports: Record<string, {dev: Runtime.Port, content: Runtime.Port}> = {};
+ 
+export default defineBackground(() => {
+  browser.runtime.onConnect.addListener((p) => {
+    p.onMessage.addListener(async(m: unknown, s) => {
+      const senderId = s.sender?.tab?.id;
+      if (!(m && typeof m === 'object')) {
+        console.error(`Message ${m} is not an object`)
+        return
+      }
+      else if (!('action' in m && typeof m.action === 'string')) {
+        console.error(`No action or wrong type in message ${m}`)
+        return
+      }
+
       switch(m.action) {
         case "connect-dev":
+	   if (!('tabId' in m && typeof m.tabId === 'string')) {
+              console.error(`No tabId or wrong type in message ${m}`)
+	     return
+	   }
             console.log(`connected to dev port on tab ${m.tabId}`);
             ports[m.tabId] = {dev: p, content: ports[m.tabId]?.content};
             break;
          case "connect-content":
             console.log(`connected to content script port on tab ${p.name}`);
-            ports[s.sender.tab.id] = {dev: ports[s.sender.tab.id]?.dev, content: p};
+	   if (senderId)
+              ports[senderId] = {dev: ports[senderId]?.dev, content: p};
+	   else console.error('sender with no tabId', s);
             break;
          case "message-dev":
-            console.log(`message from content to ${ports[s.sender.tab.id]?.dev?.name} `, m)
-            ports[s.sender.tab.id].dev.postMessage(m);
+            if (senderId) {
+              console.log(`message from content to ${ports[senderId]?.dev?.name} `, m)
+	     ports[senderId].dev.postMessage(m);
+	   }
+	   else console.error('sender with no tabId', s);
             break;
          case "message-content":
+	   if (!('tabId' in m && typeof m.tabId === 'string')) {
+              console.error(`No tabId or wrong type in message ${m}`)
+	     return
+	   }
             ports[m.tabId].content.postMessage(m);
             break;
          default:
             console.log(`browser got a message from ${p.name}`, m);
             break;
     }})
-};
-
-export default defineBackground(() => {
-  browser.runtime.onConnect.addListener(
-    connected
-  );
+});
 });
