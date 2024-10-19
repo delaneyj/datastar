e7955741c0b17a725cf9f0700b622cf375886442
Delaney Gillilan <delaneygillilan@gmail.com>
2024-10-19 13:23:44 -0700

    add search api endpoint for testing

--- backends/go/site/routes_home.go
+++ backends/go/site/routes_home.go
@@ -153,6 +153,45 @@ func setupHome(router chi.Router, searchIndex bleve.Index, store sessions.Store,
 	})
 
 	router.Route("/api", func(apiRouter chi.Router) {
+		apiRouter.Get("/search", func(w http.ResponseWriter, r *http.Request) {
+			type Result struct {
+				ID    string  `json:"id"`
+				Score float64 `json:"score"`
+			}
+			query := r.URL.Query().Get("q")
+			if query == "" {
+				http.Error(w, "missing query", http.StatusBadRequest)
+				return
+			}
+
+			searchRequest := bleve.NewSearchRequest(bleve.NewQueryStringQuery(query))
+			searchRequest.Fields = []string{"*"}
+			searchRequest.Size = 10
+
+			searchResult, err := searchIndex.Search(searchRequest)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+
+			results := make([]Result, 0, len(searchResult.Hits))
+			for _, hit := range searchResult.Hits {
+				results = append(results, Result{
+					ID:    hit.ID,
+					Score: hit.Score,
+				})
+			}
+
+			b, err := json.MarshalIndent(results, "", " ")
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+
+			w.Header().Set("Content-Type", "application/json")
+			w.Write(b)
+		})
+
 		apiRouter.Route("/todos", func(todosRouter chi.Router) {
 			todosRouter.Get("/", func(w http.ResponseWriter, r *http.Request) {
 
