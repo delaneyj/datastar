86370de37c40b97a7dbe05771555acdcf15d1746
rphumulock <rphumulock@gmail.com>
2024-12-06 22:54:22 -0500

    Examples

---
+++ examples/java/src/main/java/StarFederation/Datastar/Servlets/RemoveTargetServlet.java
@@ -0,0 +1,43 @@
+package StarFederation.Datastar.Servlets;
+
+import StarFederation.Datastar.ServerSentEventGenerator;
+import StarFederation.Datastar.adapters.response.AbstractResponseAdapter;
+import StarFederation.Datastar.adapters.response.HttpServletResponseAdapter;
+import StarFederation.Datastar.events.RemoveFragments;
+import StarFederation.Datastar.events.RemoveFragmentsOptions;
+import jakarta.servlet.AsyncContext;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.util.UUID;
+
+@WebServlet(name = "RemoveTargetServlet", urlPatterns = "/removeTarget", asyncSupported = true)
+public class RemoveTargetServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) {
+        try {
+            AsyncContext asyncContext = request.startAsync();
+            asyncContext.setTimeout(0);
+
+            AbstractResponseAdapter responseAdapter = new HttpServletResponseAdapter(response);
+            ServerSentEventGenerator sse = new ServerSentEventGenerator(responseAdapter);
+
+            String selector = "#target";
+            RemoveFragmentsOptions removeFragmentsOptions = RemoveFragmentsOptions.create();
+            removeFragmentsOptions.withSettleDuration(20);
+            RemoveFragments removeFragments = new RemoveFragments(selector, removeFragmentsOptions);
+            removeFragments.setEventId(UUID.randomUUID().toString());
+            sse.RemoveFragments(removeFragments);
+
+            asyncContext.complete();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+}
